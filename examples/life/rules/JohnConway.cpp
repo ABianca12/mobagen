066c4287b2b697fb030c4a8c9995cff45d44bcf2
Alexander Bianca <alexander.bianca@mymail.champlain.edu>
2024-09-10 14:16:31 -0400

    Began implementing step and count neighbors

--- examples/life/rules/JohnConway.cpp
+++ examples/life/rules/JohnConway.cpp
@@ -1,11 +1,28 @@
 #include "JohnConway.h"
 
+#include <numeric>
+
 // Reference: https://playgameoflife.com/info
 void JohnConway::Step(World& world) {
   // todo: implement
+  // world.SetNext() sets next tile in buffer to dead or alive
+  for (int i = 0; i < world.SideSize(); i++) {
+    for (int j = 0; j < world.SideSize(); j++) {
+
+    }
+  }
 }
 
 int JohnConway::CountNeighbors(World& world, Point2D point) {
-  // todo: implement
-  return 0;
+  int accumulator = 0;
+
+  for (int i = point.x - 1; i < point.x + 1; i++) {
+    for (int j = point.y - 1; j < point.y + 1; j++) {
+      if (point.x != i && point.y != j) {
+        accumulator += world.Get({i,j});
+      }
+    }
+  }
+  // accumulator is the cumulative number of neighbors by adding a boolean, ex, is there is a 3x3 grid a result 9 means all squares are filled
+  return accumulator;
 }
