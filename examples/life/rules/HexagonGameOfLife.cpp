6462d7951761e065aea9ae2033d8b232903ad62f
Gooby <zander.plays.games.12@gmail.com>
2024-09-14 20:23:14 -0400

    implemented the step function for HexagonGameOfLife.cpp and added comments to functions

--- examples/life/rules/HexagonGameOfLife.cpp
+++ examples/life/rules/HexagonGameOfLife.cpp
@@ -5,27 +5,44 @@
 #include "HexagonGameOfLife.h"
 
 void HexagonGameOfLife::Step(World& world) {
+  // Iterates through the world
+  for (int i = 0; i <world.SideSize(); i++) {
+    for (int j = 0; j <world.SideSize(); j++) {
+      const bool isCurrentSpaceAlive = world.Get({i,j});
+      const int liveNeighbors = CountNeighbors(world, {i, j});
 
+      if (isCurrentSpaceAlive) {
+        // Underpopulation rule
+        if (liveNeighbors < 2) {
+          world.SetNext({i,j}, false);
+        }
+        // Continuation rule
+        else if (liveNeighbors == 2) {
+          world.SetNext({i,j}, true);
+        } else {
+          // Anything that does not get caught by the first two statements would fall under the overpopulation rule
+          world.SetNext({i,j}, false);
+        }
+      } else {
+        // 3 alive neighbor rule
+        if (liveNeighbors == 2) {
+          world.SetNext({i,j}, true);
+        }
+      }
+    }
+  }
 }
 
 
 int HexagonGameOfLife::CountNeighbors(World& world, Point2D point) {
   int accumulator = 0;
-  int startingPoint = (point.x - 1) - static_cast<int>(point.y % 2 == 0);
+  int startingPoint = point.x - static_cast<int>(point.y % 2 == 0);
 
-  // Live neighbors on sides
-  for (int i = startingPoint; i < startingPoint + 3; i++) {
-    // Excludes self
-    if (i != point.x) {
-      if (world.Get({i, point.y})) {
-        accumulator++;
-      }
-    }
-  }
+  // All for loops iterate around the starting point with offsets inorder to find the neighbors of the point in specific regions
 
   // Live neighbors above
-  startingPoint = point.x + 1;
   for (int i = startingPoint; i < startingPoint + 2; i++) {
+    // Here the offset is -1
     if (world.Get({i, point.y - 1})) {
       accumulator++;
     }
@@ -33,8 +50,21 @@ int HexagonGameOfLife::CountNeighbors(World& world, Point2D point) {
 
   // Live neighbors below
   for (int i = startingPoint; i < startingPoint + 2; i++) {
+    // Here the offset is +1
     if (world.Get({i, point.y + 1})) {
       accumulator++;
+    }
+  }
+
+  // Live neighbors on sides
+  startingPoint = point.x - 1;
+  for (int i = startingPoint; i < startingPoint + 3; i++) {
+    // Excludes self
+    if (i != point.x) {
+      // No offset needed
+      if (world.Get({i, point.y})) {
+        accumulator++;
+      }
     }
   }
 
--- examples/life/rules/JohnConway.cpp
+++ examples/life/rules/JohnConway.cpp
@@ -3,6 +3,7 @@
 #include <numeric>
 
 void JohnConway::Step(World& world) {
+  // Iterates through the world
   for (int i = 0; i < world.SideSize(); i++) {
     for (int j = 0; j < world.SideSize(); j++) {
       // Gets the state of the current space
@@ -10,6 +11,7 @@ void JohnConway::Step(World& world) {
       // Gets the number of live neighbors to the current space
       const int liveNeighbors = CountNeighbors(world, {i, j});
 
+      // Checks if the current space is alive or not
       if (isCurrentSpaceAlive) {
         // Underpopulation rule
         if (liveNeighbors < 2) {
@@ -36,9 +38,12 @@ int JohnConway::CountNeighbors(World& world, Point2D point) {
   // accumulator is the cumulative number of neighbors by adding a boolean, ex, is there is a 3x3 grid a result 9 means all squares are filled
   int accumulator = 0;
 
+  // Iterates through the whole world
   for (int i = point.x - 1; i < point.x + 2; i++) {
     for (int j = point.y - 1; j < point.y + 2; j++) {
+      // Excludes current space from being a neighbor
       if (point.x != i || point.y != j) {
+        // Adds the boolean 1 or 0 value to the accumulator variable
         accumulator += static_cast<int>(world.Get({i,j}));
       }
     }
