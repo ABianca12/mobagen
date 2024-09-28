38b2a68b760f3d6e93d34d6245579b4038f51943
Gooby <zander.plays.games.12@gmail.com>
2024-09-28 19:29:27 -0400

    Implemented color changing for visited nodes

--- examples/maze/generators/RecursiveBacktrackerExample.cpp
+++ examples/maze/generators/RecursiveBacktrackerExample.cpp
@@ -4,11 +4,29 @@
 #include <climits>
 
 bool RecursiveBacktrackerExample::Step(World* w) {
-  // todo: implement this
+  Point2D point;
+  Point2D nextPoint;
+  Node node;
+  std::vector<Point2D> neighbors;
+
   if (stack.empty() && visited.empty()) {
     stack.push_back(randomStartPoint(w));
   }
 
+  if (!stack.empty()) {
+    point = stack.back();
+    node = w->GetNode(point);
+    neighbors = getVisitables(w, point);
+
+    if (!neighbors.empty()) {
+      w->SetNodeColor(point, Color::Blue);
+      visited[point.y][point.x] = true;
+
+      nextPoint = neighbors[getRandomNumber() % neighbors.size()];
+      stack.push_back(nextPoint);
+    }
+
+  }
   // Depth first search
   return false;
 }
@@ -54,4 +72,14 @@ std::vector<Point2D> RecursiveBacktrackerExample::getVisitables(World* w, const 
   }
 
   return visitables;
+}
+
+int RecursiveBacktrackerExample::getRandomNumber() {
+  int randomIndex = 0;
+  std::vector<int> randomNums = {72, 99, 56, 34, 43, 62, 31, 4, 70, 22, 6, 65, 96, 71, 29, 9, 98, 41, 90, 7, 30, 3, 97, 49, 63, 88, 47, 82, 91, 54, 74, 2, 86, 14, 58, 35, 89, 11, 10, 60, 28, 21, 52, 50, 55, 69, 76, 94, 23, 66, 15, 57, 44, 18, 67, 5, 24, 33, 77, 53, 51, 59, 20, 42, 80, 61, 1, 0, 38, 64, 45, 92, 46, 79, 93, 95, 37, 40, 83, 13, 12, 78, 75, 73, 84, 81, 8, 32, 27, 19, 87, 85, 16, 25, 17, 68, 26, 39, 48, 36};
+
+  if (randomIndex == randomNums.size()) {
+    randomIndex = 0;
+  }
+  return randomNums[randomIndex++];
 }
